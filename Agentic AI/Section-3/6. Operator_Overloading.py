3638598fa17ce478816f3b0c9d714ef0fd88026b
VibhavKrishnanKS <ksvibhav@gmail.com>
2025-12-14 12:33:45 +0530

    my commit in order to add strings.py file

---
+++ Agentic AI/Section-3/6. Operator_Overloading.py
@@ -0,0 +1,19 @@
+base_liquid = ["water", "milk"]
+extra_flavor = ["ginger"]
+
+full_liquid_mix = base_liquid + extra_flavor
+print(f"Full liquid mix: {full_liquid_mix}")  # Output: Full liquid mix: ['water', 'milk', 'ginger']
+# Here the + operator is overloaded to perform concatenation of two lists
+
+strong_brew = ["black tea"] * 3
+print(f"Strong brew mix: {strong_brew}")  # Output: Strong brew mix: ['black tea', 'black tea', 'black tea'] 
+
+strong_brew = ["black tea", "water"] * 2
+print(f"Strong brew mix: {strong_brew}")  # Output: Strong brew mix: ['black tea', 'water', 'black tea', 'water']
+strong_brew = ["black tea", "water"] * 0
+print(f"Strong brew mix: {strong_brew}")  # Output: Strong brew mix: [] 
+strong_brew = ["black tea", "water"] * -2
+print(f"Strong brew mix: {strong_brew}")  # Output: Strong brew mix: []
+strong_brew = ["black tea"] * 1 + ["water"] * 2
+print(f"Strong brew mix: {strong_brew}")  # Output: Strong brew mix: ['black tea', 'water', 'water']
+
